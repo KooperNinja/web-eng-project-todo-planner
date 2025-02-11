eef194ca38e08cf508f4dd56038f5cb95df6e73e
Sven Schräer <sven@schraeer.com>
2025-02-11 18:30:46 +0100

    feat(backend): add json middleware, add link to startup log

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,16 @@ import express from 'express'
 import { addContextToRequest } from './middleware/context.js'
 
 const app = express()
-// context might not be parsed
-app.use(addContextToRequest)
 
-app.get("/", (req, res) => {
-    res.send({
-        todoCount: req.context.prisma.todo.count()
-    })
+app.use(addContextToRequest)
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("You found us.")
 })
 
-
 app.listen(3001, '0.0.0.0', () => {
-    console.log("Server running on 3001")
+    console.log("Server running on: http://localhost:3001")
 })
 
 console.log("Setup")
