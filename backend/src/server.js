6622604a8ee31cb165cf14bbc9e21259376811bd
Sven Schräer <sven@schraeer.com>
2025-02-18 14:38:39 +0100

    chore(backend): add comments to server.js for clear inner file structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,17 @@ import todoRoutes from './routes/todos.js'
 
 const app = express()
 
+// Middleware
 app.use(addContextToRequest)
 app.use(express.json())
 
+// Routes
 app.get('/', (req, res) => {
     res.send("You found us.")
 })
-
 app.use('/api/todos', todoRoutes)
 
+// Start the Backend App
 app.listen(3001, '0.0.0.0', () => {
     console.log("Server running on: http://localhost:3001")
 })
-
-console.log("Setup")
