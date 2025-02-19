3025c352cd20e627585d30d6ae8a96f87857b0f9
Julian <julian.greiner@porsche.de>
2025-02-19 14:25:07 +0100

    feat: header-component

---
+++ frontend/scripts/components/header-component.js
@@ -0,0 +1,46 @@
+import { LitElement, html, css } from 'lit';
+
+class HeaderComponent extends LitElement {
+  static styles = css`
+    header {
+      background-color: #333;
+      color: white;
+      padding: 20px;
+      display: flex;
+      justify-content: space-between;
+      text-align: left;
+      font-size: 24px;
+      font-family: Arial, sans-serif;
+    }
+
+    nav {
+      display: flex;
+      gap: 25px;
+    }
+
+    nav a {
+      color: white;
+      text-decoration: none;
+    }
+
+    nav a:hover {
+      text-decoration: underline;
+    }
+  `;
+
+  render() {
+    return html`
+      <header>
+        <div>To-Do Planner</div>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/xxx">XXX</a>
+          <a href="/contact">Contact</a>
+          <a href="/login">Login</a>
+        </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define('header-component', HeaderComponent);
