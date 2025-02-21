0c037d30f8fd0b4382993c41092531eeb75a4171
Sven Schräer <sven@schraeer.com>
2025-02-21 01:30:33 +0100

    feat(frontend): connect loginComponent to the api

--- frontend/src/scripts/axios.js
+++ frontend/src/scripts/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-
 const backendAxios = axios.create({
     baseURL: 'http://localhost:3001/api/',
     timeout: 2000,
+    responseType: 'json',
 })
 
-return { backendAxios }
+export { backendAxios }
