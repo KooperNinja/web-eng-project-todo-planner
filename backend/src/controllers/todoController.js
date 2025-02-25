8dbe855ee574edde2f0510698c45123d297fba27
Sven Schräer <sven@schraeer.com>
2025-02-25 08:48:41 +0100

    feat(backend): add endAt to todo model

--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -27,7 +27,8 @@ export const createTodo = async (req, res) => {
                 title: title,
                 description: description ?? "",
                 startAt: new Date(startAtMs ?? Date.now()), //if startAtMs is not given, make it start now
-                duration: duration ?? 30
+                duration: duration ?? 30,
+                endAt: new Date(startAtMs + duration * 60000)
             }
         })
         res.json(newTodo)
@@ -59,7 +60,8 @@ export const updateTodo = async (req, res) => {
                 title: title || undefined,
                 description: description || undefined,
                 startAt: new Date(startAtMs) || undefined,
-                duration: duration || undefined
+                duration: duration || undefined,
+                endAt: duration && startAtMs ? new Date(startAtMs + duration * 60000) : undefined
             }
         })
         res.json(todo)
@@ -94,5 +96,69 @@ export const deleteTodo = async (req, res) => {
             error: `Todo with id = ${id} could not be deleted`,
             id: Number(id)
         })
+    }
+}
+
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ */
+export const createTodoSmartSchedule = async (req, res) => {
+    const { title, description, duration } = req.body
+    const startAtMs = Date.now()
+    try {
+        const newTodo = await req.context.prisma.todo.create({
+            data: {
+                ownerId: req.context.user.id,
+                title: title,
+                description: description ?? "",
+                startAt: new Date(startAtMs),
+                duration: duration ?? 30,
+                endAt: new Date(startAtMs + duration * 60000)
+            }
+        })
+        res.json(newTodo)
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({
+            error: "Could not create Todo with given parameters",
+            params: req.body
+        })
+    }    
+} 
+
+/**
+ * 
+ * @param {import('types/express').Context} context
+ * @param {number} duration 
+ * @param {number} timeFrameStartMs
+ * @param {number} timeFrameEndMs
+ */
+const calculateSmartStartTime = async (context, duration, timeFrameStartMs, timeFrameEndMs) => {
+    const todos = await context.prisma.todo.findMany({
+        select: {
+            startAt: true,
+            duration: true
+        },
+        where: {
+            ownerId: context.user.id,
+            OR: [
+                {
+                    startAt: {
+                        gte: new Date(timeFrameStartMs),
+                        lte: new Date(timeFrameEndMs)
+                    }
+                },
+            ]
+            
+        }
+    })
+    let startAt = new Date(timeFrameStartMs)
+    for(const todo of todos) {
+        const endAt = startAt.getTime() + duration * 60000
+        if(todo.startAt.getTime() < endAt) {
+            startAt = new Date(todo.startAt.getTime() + todo.duration * 60000)
+        }
     }
 }
